efd0fc04ca43898cbde74e4042601a1f470d0082
Eirik Hanasand <eirik.hanasand@gmail.com>
2023-03-10 12:39:24 +0100

    Initial commit

---
+++ functions/sendNotification.mjs
@@ -0,0 +1,37 @@
+import fetch from "node-fetch";
+
+/**
+ * Posts notification to FCM.
+ * @param {string} title Notification title
+ * @param {string} body  Notification body
+ * @param {string} topic Notification topic
+ */
+export default function sendNotification(title, body, topic) {
+    const key = "AAAA1H1qOow:APA91bEyRHQ7-VrtYaM4V0qXTe2TZsK8hJ73Gjvha0nc3-3wMI3_T073zgTPFbI5m0LOjQtLM-gcfGffoHnskrHfAc9xoQTUeFoGu6fqVgCWEjosLl6e4vLgAEbxDA-j2Zyn9TwwGiLe";
+    const url = "https://fcm.googleapis.com/fcm/send";
+
+    const notification = {
+        title: title,
+        body: body,
+    };
+    
+    const message = {
+        to: `/topics/${topic}`,
+        notification: notification  
+    };
+    
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `key=${key}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(message)
+    };
+
+    fetch(url, options)
+    .then(response => {
+        if(!response.ok) throw new Error("Network response error.");
+        console.log(`Successfully sent notification to topic ${topic}!`);
+    }).catch(e => {console.error("Error sending request:", e)});
+}
