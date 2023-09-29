36f108cc6152f4febe490656af5891d9eda353c5
eirikhanasand <eirik.hanasand@gmail.com>
2023-09-29 16:39:13 +0200

    Removed module and updated ts file

--- src/functions/clean.ts
+++ src/functions/clean.ts
@@ -1,5 +1,5 @@
 import { readdirSync, statSync, unlinkSync } from "fs"
-import { join } from "path"
+import { basename, join } from "path"
 
 /**
  * Removes all JS files in the repository. Meant to cleanup after tsc.
@@ -8,11 +8,20 @@ function removeJsFiles(dir: string) {
     console.log(dir)
     readdirSync(dir).forEach((file) => {
         const filePath = join(dir, file)
+        const fileBaseName = basename(filePath);
+
+        // Skipping node_modules, distribuiton and git folders
+        if (fileBaseName === "node_modules" || fileBaseName === "dist" || fileBaseName === ".git") {
+            console.log("Skipping directory:", filePath);
+            return;
+        }
 
         if (statSync(filePath).isDirectory()) {
             // If it's a directory, recursively call the function
             removeJsFiles(filePath)
-        } else if (file.endsWith('.js')) {
+        }
+        
+        if (file.endsWith('.js') && !file.endsWith('secrets.js') && !file.endsWith('clean.js')) {
             // If it's a .js file, remove it
             unlinkSync(filePath)
             console.log(`Removed: ${filePath}`)
