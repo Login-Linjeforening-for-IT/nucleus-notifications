2fbf889164ab1cb2f74ffdb1ddfae39de7276dfa
eirikhanasand <eirik.hanasand@gmail.com>
2023-09-29 16:37:31 +0200

    Removed file

--- .secrets.ts
+++
@@ -1,2 +0,0 @@
-export const api_key = "AAAA1H1qOow:APA91bG9Kqor4TCu7YJ81vQH7B_FUlsmwTDSNUYufn4RASh3Zd3v4-nLlSWLQsGW6vWqsbE9KiXkS-c-6x9nUDQOct0x4cwkY2uh8sFlfiRDfNERCU06OcRJ17OOnxRiapsAKcAQI-_z"
-export const api_url = "https://fcm.googleapis.com/fcm/send"
