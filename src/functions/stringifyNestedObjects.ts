7576641aa7f62ce66d62f0f534ba5e40a43aa8b3
eirikhanasand <eirik.hanasand@gmail.com>
2024-02-24 07:48:21 +0100

    Added comment

--- src/functions/stringifyNestedObjects.ts
+++ src/functions/stringifyNestedObjects.ts
@@ -3,9 +3,10 @@ export default function handleNestedObjects(data: { [key: string]: any } | undef
 
     for (const key in data) {
         if (typeof data[key] === 'object' && data[key] !== null) {
-            // Recursively handle nested objects
+            // Recursively handles nested objects
             handleNestedObjects(data[key])
         } else {
+            // Changes the type to string if null, number or boolean.
             if (data[key] === null) data[key] = 'null'
             if (typeof data[key] === 'number' || typeof data[key] === 'boolean') {
                 data[key] = data[key].toString()
